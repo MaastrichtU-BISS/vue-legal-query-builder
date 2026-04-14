3e68661ee1c0e3c21943a1ce859d01af1ed2ae2a
Carlos <carloseam94@gmail.com>
2026-04-14 10:07:15 +0200

    feat: define guided form types and enums

--- src/components/types.ts
+++ src/components/types.ts
@@ -5,10 +5,43 @@ export enum FormType {
   GUIDED = 'guided',
 }
 
+export enum BlockType {
+  ARTICLE_FIELD = 'ArticleField',
+  DATASET_SELECTOR = 'DatasetSelector',
+  DATE_RANGE = 'DateRange',
+  DOC_TYPE_SELECTOR = 'DocTypeSelector',
+  DOMAINS_SELECTOR = 'DomainsSelector',
+  ECLIS_INPUT = 'EclisInput',
+  IMPORTANCE_LEVEL_SELECTOR = 'ImportanceLevelSelector',
+  INSTANCES_SELECTOR = 'InstancesSelector',
+  KEYWORDS_INPUT = 'KeywordsInput',
+  NETWORK_DEGREES = 'NetworkDegrees',
+  TEXT_INPUT = 'TextInput',
+}
+
+export interface Step {
+  title: string
+  description: string
+  required: boolean
+  blockType: BlockType
+}
+
+export interface Goal {
+  title: string
+  description: string
+  icon?: string
+  steps: Step[]
+}
+
+export interface GuidedStructure {
+  goals: Goal[]
+}
+
 export interface LegalDocsFormProps {
   title?: string
   subtitle?: string
   type?: FormType
+  guidedStructure?: GuidedStructure
   onSubmit?: (data: QueryParameters) => Promise<any>
 }
 
